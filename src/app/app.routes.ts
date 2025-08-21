0369fa6beb46365903a99547c27bf8b732b3ad5d
Mohamed Gaber <63926336+DevMohamedGaber@users.noreply.github.com>
2025-08-21 22:14:25 +0300

    Remove unused Rollup packages from package-lock.json and update routing in app.routes.ts to include a NotFoundComponent for handling 404 errors.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
-
-
   { path: '', redirectTo: 'Home', pathMatch: 'full' },
   { path: 'Home', component: HomeComponent },
-  { path: '**', redirectTo: 'Home' }
+  { path: '404', component: NotFoundComponent },
+  { path: '**', component: NotFoundComponent }
 ];
